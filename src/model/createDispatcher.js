4627892abadc387d9ab2927457b500873b13fc0c
Avishek Biswas <avibiswas.uiuc@gmail.com>
2023-02-06 15:45:04 +0530

    Tie toolhandler to a dispatcher instance

--- src/model/createDispatcher.js
+++ src/model/createDispatcher.js
@@ -1,9 +1,12 @@
 import { Dispatcher } from 'flux';
+import ToolHandler from '../actions/ToolHandler';
 
 const createDispatcher = (model, ...reactions) => {
   const dispatcher = new Dispatcher();
+  const toolHandler = new ToolHandler(model);
+
   reactions.forEach((reaction) => {
-    dispatcher.register(reaction(model));
+    dispatcher.register(reaction(model, toolHandler));
   });
   return (action) => dispatcher.dispatch(action);
 };
